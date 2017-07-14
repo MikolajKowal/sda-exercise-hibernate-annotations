69db63d9ca3b97dea940b2ad58e45f86d94691ca
Szymon Napierała <szymon.napierala@wercia.pl>
2017-07-14 19:57:44 +0200

    cwiczenie 8

--- src/main/java/pl/sda/exercise/hibernate/model/Book.java
+++ src/main/java/pl/sda/exercise/hibernate/model/Book.java
@@ -2,9 +2,12 @@ package pl.sda.exercise.hibernate.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -26,6 +29,10 @@ public class Book {
 	@Column(name="title")
 	private String title;
 	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="author_id")
+	private Author bookAuthor;
+	
 	
 	public Integer getId() {
 		return id;
@@ -40,6 +47,13 @@ public class Book {
 		this.title = title;
 	}
 	
+	
+	public Author getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(Author bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
 	@Override
 	public String toString() {
 		return "Book [id=" + id + ", title=" + title + "]";
